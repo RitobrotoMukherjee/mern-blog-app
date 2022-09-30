19005aad7353ff6371a08c98c76e0310740bc817
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-09-30 11:06:29 +0530

    Fix design annomoly

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './assets/App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('blog-app'));
 root.render(
